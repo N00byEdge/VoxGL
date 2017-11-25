cabf3c39c30ee751ad8535f677774b32bf80c312
N00byEdge <me@hannesbredberg.se>
2017-11-25 06:31:11 +0100

    There is no need for hashes here anymore
    I'm making this change quite early because I think that the more stable
    we can keep the worldgen code, the better.

--- VoxGL/PerlinNoise.hpp
+++ VoxGL/PerlinNoise.hpp
@@ -15,7 +15,7 @@ inline float perlinNoise(BlockCoord x, BlockCoord y, BlockCoord seed, int instan
 	BlockCoord coordMask = ~((~0) ^ 1 << (sizeof(BlockCoord) * 8 - 1));
 
 	for (int octave = sizeof(BlockCoord) * 8 - resolution; --octave; coordMask >>= 1) {
-		auto mtseed{ std::hash<int>{}(x & coordMask) ^ std::hash<int>{}(y & coordMask) ^ std::hash<int>{}(seed) ^ std::hash<int>{}((int)instance) ^ std::hash<int>{}(octave) };
+		auto mtseed{ (x & coordMask) ^ (y & coordMask) ^ (seed) ^ ((int)instance) ^ (octave) };
 		std::mt19937_64 mt(mtseed);
 		result += std::uniform_real_distribution<float>(-range, range)(mt) / (1 << octave);
 	}
