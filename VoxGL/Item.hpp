fc1dc6663cb45f837d6586b68df436cb7b31f937
N00byEdge <me@hannesbredberg.se>
2018-09-18 18:36:37 +0200

    Change item operator+= to use pointers to avoid throwing exception

--- VoxGL/Item.hpp
+++ VoxGL/Item.hpp
@@ -5,17 +5,17 @@
 
 struct Item {
   virtual ~Item() { }
-  virtual void operator+=(Item &) { }
+  virtual void operator+=(Item *) { }
   virtual operator bool() { return false; }
 };
 
 template<typename BlockT, int maxStack = 64>
 struct BlockItem: Item {
-  void operator+=(Item &item) override {
-    auto other = dynamic_cast<BlockItem &>(item);
+  void operator+=(Item *item) override {
+    auto other = dynamic_cast<BlockItem *>(item);
     if (other) {
-      if (auto sum = num + other.num; sum > maxStack)
-        other.num = maxStack - num, num = maxStack;
+      if (auto sum = num + other->num; sum > maxStack)
+        other->num = maxStack - num, num = maxStack;
       else num = sum;
     }
   }
